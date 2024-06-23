4ed0b3e8ddc598027f7975bbe033d802a6ddb3af
bklaing2 <bklaing2>
2024-06-23 19:46:49 +0100

    Added redirect from Spotify auth

--- src/routes/auth/sign-in/+page.server.ts
+++ src/routes/auth/sign-in/+page.server.ts
@@ -1,24 +1,28 @@
+import { SPOTIFY_REDIRECT } from "$env/static/private";
 import { redirect } from '@sveltejs/kit'
 
 
 const SCOPES = [
-  'user-follow-read',
-  'playlist-read-private',
-  'playlist-modify-public',
-  'playlist-modify-private',
-  'ugc-image-upload'
+	'user-follow-read',
+	'playlist-read-private',
+	'playlist-modify-public',
+	'playlist-modify-private',
+	'ugc-image-upload'
 ]
 
 
 export const actions = {
 	default: async ({ locals }) => {
-    const { data, error } = await locals.supabase.auth.signInWithOAuth({
-      provider: 'spotify',
-      options: { scopes: SCOPES.join(' ') }
-    })
+		const { data, error } = await locals.supabase.auth.signInWithOAuth({
+			provider: 'spotify',
+			options: {
+				scopes: SCOPES.join(' '),
+				redirectTo: SPOTIFY_REDIRECT
+			}
+		})
 
-    
-    if (error) throw new Error(error.message)
-    throw redirect(303, data.url as string)
-  }
+
+		if (error) throw new Error(error.message)
+		throw redirect(303, data.url as string)
+	}
 }
