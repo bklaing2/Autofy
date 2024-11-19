f9c57726874a267d97eed6001071e3651c8ca286
Bryceson Laing <bklaing2@gmail.com>
2024-11-18 21:03:23 -0600

    remove supabase

--- src/routes/auth/sign-in/+page.server.ts
+++ src/routes/auth/sign-in/+page.server.ts
@@ -1,28 +1,24 @@
-import { SPOTIFY_REDIRECT } from "$env/static/private";
+import { SPOTIFY_CLIENT_ID, SPOTIFY_REDIRECT_URI } from "$env/static/private";
+import type { Actions } from './$types';
 import { redirect } from '@sveltejs/kit'
 
 
 const SCOPES = [
-	'user-follow-read',
-	'playlist-read-private',
-	'playlist-modify-public',
-	'playlist-modify-private',
-	'ugc-image-upload'
+  'user-follow-read',
+  'playlist-read-private',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'ugc-image-upload'
 ]
 
 
 export const actions = {
-	default: async ({ locals }) => {
-		const { data, error } = await locals.supabase.auth.signInWithOAuth({
-			provider: 'spotify',
-			options: {
-				scopes: SCOPES.join(' '),
-				redirectTo: SPOTIFY_REDIRECT
-			}
-		})
+  default: async ({ locals }) => {
+    const { spotify } = locals
+    spotify.setClientId(SPOTIFY_CLIENT_ID)
+    spotify.setRedirectURI(SPOTIFY_REDIRECT_URI)
 
-
-		if (error) throw new Error(error.message)
-		throw redirect(303, data.url as string)
-	}
-}
+    const authorizeUrl = spotify.createAuthorizeURL(SCOPES, 'login')
+    throw redirect(303, authorizeUrl)
+  }
+} satisfies Actions
