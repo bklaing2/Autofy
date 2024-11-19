3d269edb165d3f80dc7e077473813db74bea69f6
Bryceson Laing <bklaing2@gmail.com>
2024-11-18 21:59:32 -0600

    fix db casing

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,16 +1,9 @@
 import { sql } from 'drizzle-orm';
-import { boolean, char, integer, pgTable, text, timestamp, varchar, uuid } from 'drizzle-orm/pg-core';
-
-export const usersTable = pgTable('users', {
-  id: uuid().primaryKey().default(sql`gen_random_uuid()`),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+import { boolean, char, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const playlistsTable = pgTable('playlists', {
   id: char({ length: 22 }).primaryKey().notNull(),
-  userId: uuid().notNull().references(() => usersTable.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
+  userId: text().notNull(),
   artists: text().array().notNull().default(sql`ARRAY[]::text[]`),
   followedArtists: text().array().notNull().default(sql`ARRAY[]::text[]`),
   updateWhenArtistPosts: boolean().notNull().default(false),
@@ -20,7 +13,7 @@ export const playlistsTable = pgTable('playlists', {
 });
 
 export const tokensTable = pgTable('tokens', {
-  userId: uuid().primaryKey().notNull().references(() => usersTable.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
+  userId: text().primaryKey().notNull(),
   refreshToken: text().notNull(),
   accessToken: text().notNull(),
 });
