fd4870768761407281abfbf6c3ebd4c18bfb0605
Bryceson Laing <bklaing2@gmail.com>
2024-11-30 01:09:06 -0600

    update schema

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,8 @@
 import { DATABASE_URL } from "$env/static/private";
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from './schema';
 
-const db = drizzle({ connection: DATABASE_URL, casing: 'snake_case' });
+const db = drizzle({ schema, connection: DATABASE_URL, casing: 'snake_case' });
 
 export default db
 export type Database = typeof db
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,19 @@
-import { sql } from 'drizzle-orm';
+import { relations, sql } from 'drizzle-orm';
 import { boolean, char, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const usersTable = pgTable('users', {
+  id: text().primaryKey().notNull(),
+  refreshToken: text().notNull(),
+  accessToken: text().notNull(),
+});
+
+export const usersRelations = relations(usersTable, ({ many }) => ({
+  playlists: many(playlistsTable),
+}));
 
 export const playlistsTable = pgTable('playlists', {
   id: char({ length: 22 }).primaryKey().notNull(),
-  userId: text().notNull(),
+  userId: text().notNull().references(() => usersTable.id, { onDelete: 'cascade' }),
   artists: text().array().notNull().default(sql`ARRAY[]::text[]`),
   followedArtists: text().array().notNull().default(sql`ARRAY[]::text[]`),
   updateWhenArtistPosts: boolean().notNull().default(false),
@@ -12,9 +22,6 @@ export const playlistsTable = pgTable('playlists', {
   updatedAt: timestamp().notNull().defaultNow(),
 });
 
-export const tokensTable = pgTable('tokens', {
-  userId: text().primaryKey().notNull(),
-  refreshToken: text().notNull(),
-  accessToken: text().notNull(),
-});
-
+export const playlistsRelations = relations(playlistsTable, ({ one }) => ({
+  user: one(usersTable, { fields: [playlistsTable.userId], references: [usersTable.id] }),
+}));
