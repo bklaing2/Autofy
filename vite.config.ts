64acd12bf97d1d5bc00929d6cb27f06aebbfe336
Bryceson Laing <bklaing2@gmail.com>
2024-11-30 16:22:07 -0600

    dockerize frontend for dev

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite'
 
 export default defineConfig({
-	plugins: [ sveltekit(), Icons({ compiler: 'svelte', autoInstall: true }) ]
+  plugins: [sveltekit(), Icons({ compiler: 'svelte', autoInstall: true })],
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 });
