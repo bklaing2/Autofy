d64f44572990029c9d86889f1495427f5107fcde
Bryceson Laing <bklaing2@gmail.com>
2025-01-26 17:24:11 -0600

    change Playlist.UpdatedAt from int to Time

--- lambdas/util/models/db.go
+++ lambdas/util/models/db.go
@@ -1,15 +1,17 @@
 package models
 
+import "time"
+
 type Playlist struct {
-	ID                            string   `json:"id"`
-	UserID                        string   `json:"user_id"`
-	Artists                       []string `json:"artists"`
-	FollowedArtists               []string `json:"followed_artists"`
-	IncludeFollowedArtists        bool     `json:"include_followed_artists"`
-	UpdateWhenArtistPosts         bool     `json:"update_when_artist_posts"`
-	UpdateWhenUserFollowsArtist   bool     `json:"update_when_user_follows_artist"`
-	UpdateWhenUserUnfollowsArtist bool     `json:"update_when_user_unfollows_artist"`
-	UpdatedAt                     int      `json:"updated_at"`
+	ID                            string    `json:"id"`
+	UserID                        string    `json:"user_id"`
+	Artists                       []string  `json:"artists"`
+	FollowedArtists               []string  `json:"followed_artists"`
+	IncludeFollowedArtists        bool      `json:"include_followed_artists"`
+	UpdateWhenArtistPosts         bool      `json:"update_when_artist_posts"`
+	UpdateWhenUserFollowsArtist   bool      `json:"update_when_user_follows_artist"`
+	UpdateWhenUserUnfollowsArtist bool      `json:"update_when_user_unfollows_artist"`
+	UpdatedAt                     time.Time `json:"updated_at"`
 }
 
 type User struct {
